e3de79894a8f07ef8b8207e5b2896382e17c4099
Abbes Achaq <aachaq@e2r10p1.1337.ma>
2021-11-16 23:55:02 +0100

    push

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,20 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aachaq <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/15 23:42:38 by aachaq            #+#    #+#             */
-/*   Updated: 2021/11/16 22:35:03 by aachaq           ###   ########.fr       */
+/*   Created: 2021/11/16 22:44:51 by aachaq            #+#    #+#             */
+/*   Updated: 2021/11/16 23:30:24 by aachaq           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
 
-int	ft_isalnum(int c)
+size_t	ft_strlen(const	char	*s)
 {
-	if (((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		|| (c >= '0' && c <= '9'))
-		return (1);
-	else
-		return (0);
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+int main(void)
+{
+	const 	char	i[5] = "abcd";
+	printf("%zu\n", ft_strlen(i));
+	printf("%zu\n", strlen(i));
+	return (0);
 }
